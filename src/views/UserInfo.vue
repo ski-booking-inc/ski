bcdceea3edc146b80c0906009feddb2bbba806c7
Jimmy Svensson <jimmy888swe@gmail.com>
2019-03-08 15:05:34 +0100

    huvudvrk

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -6,34 +6,35 @@
   <form class="text"> 
          <div>
           <label for="form_firstname">Förnamn </label>
-          <input id="form_firstname" type="text" name="firstname" placeholder="Förnamn *" required="required" data-error=" Lägg till förnamn." v-model="userInput.firstName">
+          <input id="form_firstname" type="text"  value="Förnamn" placeholder="Förnamn *" required="required" data-error=" Lägg till förnamn." v-model="userInput.firstName">
          </div>
          <div>
           <label for="form_lastname">Efternamn </label>
-          <input id="form_lastname" type="text" name="surname" placeholder="Efternamn *" required="required" data-error=" Lägg till efternamn." v-model="userInput.lastName">
+          <input id="form_lastname" type="text" value="Efternamn" placeholder="Efternamn *" required="required" data-error=" Lägg till efternamn." v-model="userInput.lastName">
          </div>
         <hr/>
          <div>
           <label for="form_weight">Vikt </label>
-          <input id="form_weight" type="Number" name="weight" placeholder="Vikt *" required="required" data-error=" Lägg till din vikt." v-model="userInput.weight">
+          <input id="form_weight" type="Number" value="Vikt" placeholder="Vikt *" required="required" data-error=" Lägg till din vikt." v-model="userInput.weight">
          </div>
          <div>
           <label for="form_lenght">Längd </label>
-          <input id="form_length" type="Number" name="lenght" placeholder="Längd *" required="required" data-error=" Lägg till din längd." v-model="userInput.lenght">
+          <input id="form_length" type="Number" value="Läng" placeholder="Längd *" required="required" data-error=" Lägg till din längd." v-model="userInput.length">
          </div>
          <div>
           <label for="form_shoe">Skostorlek </label>
-          <input id="form_shoe" type="text" name="Number" placeholder="Skostorlek *" required="required" data-error=" Lägg till din skostorlek." v-model="userInput.shoe">
+          <input id="form_shoe" type="Number" value="shoe" placeholder="Skostorlek *" required="required" data-error=" Lägg till din skostorlek." v-model="userInput.shoe">
          </div>
   </form>
   <form class="box">
     <div>
-      <h3>Hjälm? | Gratis</h3><input type="checkbox"  value="helmet" name="helmet" id="form_helmet" v-model="userInput.helmet"><br>
-      <h3>Skidglasögon? | + 49:-</h3><input type="checkbox" value="skigoogles" name="skigoogles" id="form_skigoogles" v-model="userInput.skigoogles"><br>
-      <h3>Liftkort? | + 1099 :-</h3><input type="checkbox" value="lift" name="lift" id="form_lift" v-model="userInput.lift"><br>
+      <h3>Hjälm? | Gratis</h3><input type="checkbox"  value="helmet"  v-model="userInput.helmet"><br>
+      <h3>Skidglasögon? | + 49:-</h3><input type="checkbox" value="skigoogles"  v-model="userInput.skigoogles"><br>
+      <h3>Liftkort? | + 1099 :-</h3><input type="checkbox" value="lift"  v-model="userInput.lift"><br>
     </div>
   </form>
-    {{userInput}}
+  {{userInput}}
+  <div><input type="button" value="Lägg till" @click="inputFromUser()"></div>
   </main>
 </template>
 
@@ -44,10 +45,25 @@ export default {
   name: 'userInfo',
   data() {
     return{
-      userInput: {}
+      userInput: {
+        firstName: '',
+        lastName: '',
+        weight: null,
+        length: null,
+        shoe:null,
+        helmet: false,
+        skigoogles: false,
+        lift: false,
+      }
     }
-  }
+  },
+  methods: {
+    inputFromUser: function (userInput){
+      console.log(this.userInput);
+      }
 }
+}
+
 </script>
 
 <style lang="scss" scoped>
