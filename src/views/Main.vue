3255e50db4559ceeeba35796d71a6cf01d29c225
reeklof <reeklof@live.com>
2019-03-10 18:09:20 +0100

    design on mainpage

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="main">
     <h1>Main</h1>
+    <section>
+      <h1>Välkommen till Fjällgårdens skidanläggning</h1>
+      <p>Hyr din utrustning hos oss på Fjällgården och få mer utav din semester. Vi anpassar utrustningen efter dina behov och du hämtar enkelt ut den precis vid backen.</p>
+    </section>
+    <section>
+      <a class="btn" href="#" @click="$router.push('/products')">Boka utrustning</a>
+      <a class="btn" href="#" @click="$router.push('/userpage')">Mina bokningar</a>
+    </section>
+    <section class="logo">
+      <img src="../assets/Vector.png" alt="admin" @click="$router.push('/admin')">
+    </section>
+    <router-view/>
   </div>
 </template>
+
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
-<style lang="sass">
+<style lang="scss">
+@import "../scss/main";
+
+.main {
+  margin: 0 1.5rem;
+  @extend %center;
+  flex-direction: column;
+  max-height: 100vh;
+  color: white;
+}
+
+.logo {
+  align-self: start;
+  margin-top: 3rem;
+}
 
 </style>
