e8210305ecd1c306d8beec0d55a17c9c101562b4
Sandra <otterstensandra@gmail.com>
2019-03-11 14:18:32 +0100

    Working on product component

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
-    <section>
-      <h2>{{ chosenProduct.article }}</h2>
-      <p>{{ chosenProduct.brand }}</p>
-      <p>{{  chosenProduct.category }}</p>
-      <p>{{  chosenProduct.age }}</p>
-      <p>{{ chosenProduct.info }}</p
-      <h3>{{ chosenProduct.price }}</h3>
-    </section>
-    <section>
+      <section>
+        <h2>{{ userInfo.name }}</h2>
+        <p>{{ chosenProduct.article }}</p>
+      </section>
+      <section>
+        <p>{{ chosenProduct.brand }}</p>
+        <p>{{  chosenProduct.category }}</p>
+        <p>{{  chosenProduct.age }}</p>
+        <p>{{ chosenProduct.info }}</p>
+        <h3>{{ chosenProduct.price }}</h3>
+      </section>
+      <section>
 
-    </section>
+      </section>
   </main>
 </template>
 
