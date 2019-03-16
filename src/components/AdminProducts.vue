8c293e0a46151ed0e75e2efe793a1750d86d1834
reeklof <reeklof@live.com>
2019-03-16 12:09:44 +0100

    input search field for better product overview

--- src/components/AdminProducts.vue
+++ src/components/AdminProducts.vue
@@ -1,48 +1,70 @@
 <template>
-          <div class="productAdmin adminTable">
-        <div>
-            <h3>Products</h3>
-              <table cellspacing="0" class="table">
-                  <thead>
-                      <tr>
-                          <th>Namn:</th>
-                          <th>Nivå:</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr v-for="prod in products" :key="prod._id">
-                      <td>{{ prod.article }}</td>
-                      <td>{{ prod.category }}</td>
-                      <!-- <td><button><img src="../assets/img/edit.svg" alt="edit"></button></td>  -->
-                      <td><button @click="removeProd(prod._id)"><img src="../assets/img/baseline-delete-24px.svg" alt="Ta Bort"></button></td>
-                      </tr>
-                  </tbody>
-              </table>
-        </div>
-      </div>
+  <div class="productAdmin adminTable">
+    <div>
+      <h3>Produkter</h3>
+      <input class="inputsearch" type="text" v-model="search" placeholder="Sök efter en produkt">
+      <table cellspacing="0" class="table">
+        <thead>
+          <tr>
+            <th>Namn:</th>
+            <th>Nivå:</th>
+            <th>Ålder:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="prod in filterProd" :key="prod._id">
+            <td>{{ prod.article }}</td>
+            <td>{{ prod.category }}</td>
+            <td>{{ prod.age }}</td>
+            <!-- <td><button><img src="../assets/img/edit.svg" alt="edit"></button></td>  -->
+            <td>
+              <button @click="removeProd(prod._id)">
+                <img src="../assets/img/baseline-delete-24px.svg" alt="Ta Bort">
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: 'AdminProducts',
-data(){
+  name: "AdminProducts",
+  data() {
     return {
+      prod: [],
+      search: ""
+    };
+  },
+  methods: {
+    async removeProd(id) {
+      this.$store.dispatch("removeProd", id);
+      await this.$store.dispatch("getProducts");
     }
-},
-methods: {
-      async removeProd(id) {
-     this.$store.dispatch('removeProd',id);
-      await this.$store.dispatch('getProducts');
-   }
-},
-computed: {
-    products(){
-        return this.$store.state.products;
-   }
- }
-}
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+    filterProd: function() {
+      return this.products.filter(prod => {
+        return (
+          prod.article.toLowerCase().match(this.search.toLowerCase()) ||
+          prod.category.toLowerCase().match(this.search.toLowerCase()) ||
+          prod.age.toLowerCase().match(this.search.toLowerCase())
+        );
+      });
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.inputsearch {
+  margin-top: 1rem;
+  font-size: 0.7rem;
+  padding: 0.2rem 1rem;
+}
 </style>
